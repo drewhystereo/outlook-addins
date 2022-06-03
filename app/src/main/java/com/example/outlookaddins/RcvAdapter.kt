2e0906830d0587847232679952aca2e5e64c1fef
drewhystereo <dhruv.kdood@gmail.com>
2022-06-04 02:21:36 +0530

    initial commit

---
+++ app/src/main/java/com/example/outlookaddins/RcvAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.outlookaddins
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.addin_layout.view.*
+
+
+class RcvAdapter (private val list: List<MyAddIns>):
+
+    RecyclerView.Adapter<RcvAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.addin_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val addin_list = list[position]
+        val context = holder.itemView.context
+        holder.title.text = addin_list.title
+        Glide.with(holder.itemView).load(addin_list.IconUrl).into(holder.icon)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ViewHolder(ItemView:View): RecyclerView.ViewHolder(ItemView){
+        val title: TextView = itemView.findViewById(R.id.txt_title)
+        val icon: ImageView = itemView.findViewById(R.id.img_icon)
+    }
+}
+
